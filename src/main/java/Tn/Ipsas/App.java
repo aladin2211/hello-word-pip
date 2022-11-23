d6daa6c485935cf525687ec80f20d09a346a2241
aladin2211 <aladinabdelkader22@gmail.com>
2022-11-23 20:39:12 +0100

    first commit

---
+++ src/main/java/Tn/Ipsas/App.java
@@ -0,0 +1,13 @@
+package Tn.Ipsas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
